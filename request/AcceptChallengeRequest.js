3dced828847e4640f0136d0156fb4c5b87b71850
James Howard <james@reetgood.co.uk>
2016-10-14 09:59:49 +0100

    Sync with CloudCode.

--- request/AcceptChallengeRequest.js
+++ request/AcceptChallengeRequest.js
@@ -1,1 +1,3 @@
 var a = 2;
+var b = a;
+var c = b;
--- rtScript/RTS2.js
+++ rtScript/RTS2.js
@@ -1,15 +1,10 @@
 // ====================================================================================================
 //
-// Cloud Code for AccountDetailsRequest, write your code here to customise the GameSparks platform.
+// Cloud Code for module, write your code here to customise the GameSparks platform.
 //
 // For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
 //
 // ====================================================================================================
 
-var logger = Spark.getLog();
-logger.debug('debug test');
-logger.info('info test');
-logger.warn('warn test');
-logger.error('error test');
-
-a('test');
+// This is my RTS. Not sure what goes here.
+var isRealtime = true;
